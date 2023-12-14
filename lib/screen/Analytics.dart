347c99bebeb9d7d109a31b5f60eca3e66470e2fa
Priyangshu Jyoti Saha <116666129+PriyangshuJS@users.noreply.github.com>
2023-12-14 20:28:39 +0530

    integrated firbase to AnalyticsPage

--- lib/screen/Analytics.dart
+++ lib/screen/Analytics.dart
@@ -1,15 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/detailHeader.dart';
 import '../widgets/tabedContainer.dart';
 
-class Analytics extends StatelessWidget {
+class Analytics extends StatefulWidget {
   const Analytics({super.key});
 
+  @override
+  State<Analytics> createState() => _AnalyticsState();
+}
+
+class _AnalyticsState extends State<Analytics> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           'Analytics',
           style: TextStyle(
@@ -86,64 +93,155 @@ class Analytics extends StatelessWidget {
           SizedBox(height: 10),
           ContainerWithTabs(),
           SizedBox(height: 20),
-          DetailBar(title: "Over All"),
+          DetailBar(title: "Reach"),
           SizedBox(height: 10),
           ContainerWithTabs(),
           SizedBox(height: 20),
-          DetailBar(title: "Over All"),
+          DetailBar(title: "Revenue"),
           SizedBox(height: 10),
           ContainerWithTabs(),
           SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width / 2.5,
-                height: MediaQuery.of(context).size.height / 7,
-                decoration: ShapeDecoration(
-                  color: Color(0xFFFFEEF1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Total earnings"),
-                    Text("2500"),
-                  ],
+              Text(
+                'Patient',
+                style: TextStyle(
+                  color: Color(0xFF0D0D0D),
+                  fontSize: 20,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width / 2.5,
-                height: MediaQuery.of(context).size.height / 7,
-                decoration: ShapeDecoration(
-                  color: Color(0xFFFFEEF1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+              SizedBox(width: MediaQuery.of(context).size.width / 4),
+              Text(
+                'Reviews',
+                style: TextStyle(
+                  color: Color(0xFF0D0D0D),
+                  fontSize: 20,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Total earnings"),
-                    Text("2500"),
-                  ],
-                ),
-              ),
+              )
             ],
           ),
+          SizedBox(height: 10),
+          StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('Analytics')
+                  .doc('BQ4SFtOPiaZY3zbRjXE2')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                final data = snapshot.data?.data() as Map<String, dynamic>;
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2.5,
+                      height: MediaQuery.of(context).size.height / 7,
+                      decoration: ShapeDecoration(
+                        color: Color(0xFFFFEEF1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Total earnings"),
+                          Text(data['tearning'] ?? 'Unknown'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2.5,
+                      height: MediaQuery.of(context).size.height / 7,
+                      decoration: ShapeDecoration(
+                        color: Color(0xFFFFEEF1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Total earnings"),
+                          Text(data['tsaving'] ?? 'Unknown'),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }),
           SizedBox(height: 20),
-          Container(
-            height: MediaQuery.of(context).size.height / 8,
-            width: MediaQuery.of(context).size.width,
-            decoration: ShapeDecoration(
-              color: Color(0xFFFFEEF1),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          )
+          StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('Analytics')
+                  .doc('BQ4SFtOPiaZY3zbRjXE2')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                final data = snapshot.data?.data() as Map<String, dynamic>;
+                var maleno = int.parse(data['male'] ?? '0');
+                var femaleno = int.parse(data['female'] ?? '0');
+                return Container(
+                    height: MediaQuery.of(context).size.height / 8,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFFFEEF1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          '${maleno + femaleno}\nTotal',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xFF0D0D0D),
+                            fontSize: 20,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              'Male : $maleno',
+                              style: TextStyle(
+                                color: Color(0xFF0D0D0D),
+                                fontSize: 20,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                              ),
+                            ),
+                            Text(
+                              'Female : $femaleno',
+                              style: TextStyle(
+                                color: Color(0xFF0D0D0D),
+                                fontSize: 20,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ));
+              })
         ]),
       )),
     );
--- lib/widgets/tabedContainer.dart
+++ lib/widgets/tabedContainer.dart
@@ -1,10 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,104 +33,122 @@ class ContainerWithTabs extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
         ),
       ),
-      child: DefaultTabController(
-        length: 4,
-        child: Column(
-          children: [
-            TabBar(
-              unselectedLabelColor: Colors.black,
-              indicatorSize: TabBarIndicatorSize.tab,
-              indicator: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFFF83D5B),
-              ),
-              tabs: [
-                Tab(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text("Today"),
+      child: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('Analytics')
+            .doc('BQ4SFtOPiaZY3zbRjXE2')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+
+          final data = snapshot.data?.data() as Map<String, dynamic>;
+          var todayValue = data['today'] ?? '0';
+          var weekValue = data['week'] ?? '0';
+          var monthValue = data['monthly'] ?? '0';
+          var lifetimeValue = data['lifetime'] ?? '0';
+
+          return DefaultTabController(
+            length: 4,
+            child: Column(
+              children: [
+                TabBar(
+                  unselectedLabelColor: Colors.black,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF83D5B),
                   ),
+                  tabs: const [
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("Today"),
+                      ),
+                    ),
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("Week"),
+                      ),
+                    ),
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("Month"),
+                      ),
+                    ),
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("Lifetime"),
+                      ),
+                    ),
+                  ],
                 ),
-                Tab(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text("Week"),
-                  ),
-                ),
-                Tab(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text("Month"),
-                  ),
-                ),
-                Tab(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text("Lifetime"),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      Center(
+                        child: Text(
+                          todayValue,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Color(0xFFF83D5B),
+                            fontSize: 36,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w700,
+                            height: 0.04,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          weekValue,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Color(0xFFF83D5B),
+                            fontSize: 36,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w700,
+                            height: 0.04,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          monthValue,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Color(0xFFF83D5B),
+                            fontSize: 36,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w700,
+                            height: 0.04,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          lifetimeValue,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Color(0xFFF83D5B),
+                            fontSize: 36,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w700,
+                            height: 0.04,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  Center(
-                    child: Text(
-                      '4880',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFF83D5B),
-                        fontSize: 36,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
-                        height: 0.04,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      '4880',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFF83D5B),
-                        fontSize: 36,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
-                        height: 0.04,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      '4880',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFF83D5B),
-                        fontSize: 36,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
-                        height: 0.04,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      '4880',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFF83D5B),
-                        fontSize: 36,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
-                        height: 0.04,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
